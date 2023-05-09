31385bcdc15bea08265c06bef3d6f705f1766e3b
YanLozovskyi <yanlozovskiy@gmail.com>
2023-05-09 12:49:12 +0300

    task-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const VIDEO_PLAYER_TIME_STORAGE_KEY = 'video-player-current-time';
+
+updateCurrentTime();
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(data) {
+  localStorage.setItem(VIDEO_PLAYER_TIME_STORAGE_KEY, data.seconds);
+  console.log(data.seconds);
+}
+
+function updateCurrentTime() {
+  const persistedData = localStorage.getItem(VIDEO_PLAYER_TIME_STORAGE_KEY);
+
+  if (!persistedData) return;
+  player.setCurrentTime(parseFloat(persistedData));
+}
